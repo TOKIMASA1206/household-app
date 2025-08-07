1336cdd55ce230fa8d65b4f6cd208c9824f584aa
TOKIMASA1206 <tokihide121062@gmail.com>
2025-08-08 03:05:19 +0900

    fix: firebase.tsを編集

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
+// firebase.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBRlM7MI6KZhxUv77m4b48FRihxamEmD-k",
-  authDomain: "household-app-f644e.firebaseapp.com",
-  projectId: "household-app-f644e",
-  storageBucket: "household-app-f644e.firebasestorage.app",
-  messagingSenderId: "791980201904",
-  appId: "1:791980201904:web:4350bd09f89f142df3e875"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY!,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID!,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
+
+console.log(process.env, "Firebase API Key Loaded");
